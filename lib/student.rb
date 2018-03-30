cd1bdc02ed5fe204d01468233a08dd9b0e0ef8ca
Kelsey Fecho <kfecho30@gmail.com>
2018-03-30 14:25:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,12 @@ class Student
   end
 
   def self.count_all_students_in_grade_9
-    self.all
-    binding.pry
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = 9
+      GROUP BY id;
+      SQL
+      binding.pry
+    DB[:conn].execute(sql)
   end
 end
