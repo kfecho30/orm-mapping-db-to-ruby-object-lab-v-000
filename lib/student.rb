6efd8b16eb33a6aee4a1f788584f14ee726de535
Kelsey Fecho <kfecho30@gmail.com>
2018-03-30 14:12:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,8 +21,6 @@ class Student
   end
 
   def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
     sql = <<-SQL
       SELECT * FROM students
       WHERE name = ?;
@@ -30,7 +28,7 @@ class Student
 
     DB[:conn].execute(sql, name).map do |row|
       Student.new_from_db(row)
-    end.first
+    end
   end
 
   def save
